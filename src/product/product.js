056bd907d1ae3c21828c3414e9f7cd8f22bef8ca
Prakhar Gupta <prakhar.gupta@mindtickle.com>
2022-07-28 15:56:30 +0530

    Update: added asd

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,9 @@
 class Product {
-    constructor(id, type, name, version, asd) {
+    constructor(id, type, name, version) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
-        this.asd = asd;
     }
 }
 
