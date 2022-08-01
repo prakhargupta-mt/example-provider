4575430d5d6cf1ac6eef7e104b4ef57b8b011b7e
Prakhar Gupta <prakhar.gupta@mindtickle.com>
2022-08-01 13:52:37 +0530

    Update: Removed price (expected field) from provider

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,9 @@
 class Product {
-    constructor(id, type, name, version, price, color) {
+    constructor(id, type, name, version, color) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
-        this.price = price;
         this.color = color
     }
 }
